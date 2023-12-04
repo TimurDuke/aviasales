d6254499e8e77bae8fbf18018a2fbe86d1476427
TimurDuke <primecsgo2018@gmail.com>
2023-12-05 02:46:22 +0900

    added link to get airlines logo

--- src/components/TicketAirlines/TicketAirlines.js
+++ src/components/TicketAirlines/TicketAirlines.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import airlineLogo from '../../assets/images/airline_logo.png';
-
-const TicketAirlines = () => <img src={airlineLogo} alt="#" />;
+const TicketAirlines = ({ carrier }) => (
+    <img src={`https://pics.avs.io/99/36/${carrier}.png`} alt="#" />
+);
 
 export default TicketAirlines;
+
+TicketAirlines.propTypes = {
+    carrier: PropTypes.string.isRequired,
+};
